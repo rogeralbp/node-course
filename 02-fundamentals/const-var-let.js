014af01edaaae2ff416c6dd7011ca5161f9d9ec6
Roger Oporta <roportaperez@gmail.com>
2022-04-09 11:56:38 -0600

    var, let, const

---
+++ 02-fundamentals/const-var-let.js
@@ -0,0 +1,14 @@
+
+
+var name = 'ROGER';
+
+console.log(name);
+
+
+/**
+ *  var   = not recommended to keep using
+ *  let   = common word to use and declare variables (eventually change)
+ *  const = variable with a value that not change (never change)
+ * 
+ * scope  = space of memory of a specific code (limits) 
+ */
