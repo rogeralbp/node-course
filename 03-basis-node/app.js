2fc4cca4ce6635a27b295ff5bdff4da64e9ff7c3
Roger Oporta <roportaperez@gmail.com>
2022-04-11 16:37:00 -0600

    Yargs, colors and npm created

--- 03-basis-node/app.js
+++ 03-basis-node/app.js
@@ -1,12 +1,35 @@
 
 
+
+const { options } = require('yargs');
 const { createFile } = require('./helpers/multiplication');
+
+const  argv  = require('yargs')
+
+                .option('b' , {
+                    alias: 'base number',
+                    type: 'number',
+                    demandOption: true
+                })
+
+                .check( (argv , options) => {
+                
+                    if( isNaN(argv.b) ){
+                        throw 'Base Number must to be a NUMBER'
+                    }
+                
+                    return true;
+                
+                } )                        
+                .argv;
 
 console.clear();
 
-const baseNumber = 36;
+console.log( argv );
+
+console.log( argv.b );
 
 
-createFile( baseNumber )
+createFile( argv.b )
     .then( fileName => console.log( fileName, 'CREATED' ) )
     .catch( error => console.log( error ) );
---
+++ 03-basis-node/npm-notes.md
@@ -0,0 +1,22 @@
+
+# NPM notes
+node package manager = packages that other devs share to the ecosystem of Node.js for it free use, generally are solutions to problems that someone faced though a development experience.
+
+## Commands
+
+- Create the file where all the necessary dependencies will be saved for a future deployment
+```
+    npm init
+```
+
+
+## package.json
+where all the dependencies will be saved for easy looking of what the app needs to works fine.
+
+## package-lock.json
+instructions of how the dependencies must tp being builded taking into account
+the version, the repo from it will be downloaded, etc.
+
+## node_modules
+folder where all the dependencies and libraries that you use in your nodejs project are stored.
+
